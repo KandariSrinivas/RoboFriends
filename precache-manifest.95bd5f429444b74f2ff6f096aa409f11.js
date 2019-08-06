7b0fa163087dd86978ca202c69125d2ebb24209a
SydneyStriker <sydneystriker4@gmail.com>
2019-08-06 20:43:28 +0530

    Updates

---
+++ precache-manifest.95bd5f429444b74f2ff6f096aa409f11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26d8ef6679f2e815e17576137bd13d4",
+    "url": "/RoboFriendsApp/index.html"
+  },
+  {
+    "revision": "52b32a6b7fe17432f022",
+    "url": "/RoboFriendsApp/static/css/main.11a7fdd4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/RoboFriendsApp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "52b32a6b7fe17432f022",
+    "url": "/RoboFriendsApp/static/js/main.4a525b71.chunk.js"
+  },
+  {
+    "revision": "40cbab871f78f77db683",
+    "url": "/RoboFriendsApp/static/js/runtime~main.9967c245.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RoboFriendsApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
